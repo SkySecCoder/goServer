53b3b13b382afa9d30082cb5096cfaeb39edd305
Aakash Prasad <aakashprasad@Legion.local>
2020-01-15 20:38:36 +0530

    Editing banner

--- goServer/banner/hello_test.go
+++ goServer/banner/hello_test.go
@@ -8,9 +8,9 @@ func TestHello(t *testing.T) {
 	expected := "\n"
 	expected += "            ____                           "
 	expected += "  __ _  ___/ ___|  ___ _ ____   _____ _ __ "
-	expected += " / _` |/ _ \___ \ / _ \ '__\ \ / / _ \ '__|"
-	expected += "| (_| | (_) |__) |  __/ |   \ V /  __/ |   "
-	expected += " \__, |\___/____/ \___|_|    \_/ \___|_|   "
+	expected += " / _` |/ _ \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|"
+	expected += "| (_| | (_) |__) |  __/ |   \\ V /  __/ |   "
+	expected += " \\__, |\\___/____/ \\___|_|    \\_/ \\___|_|   "
 	expected += " |___/\n"
 	if returnValue := Hello(); returnValue != expected {
 		t.Error("Hello() = "+returnValue+" , Needed = '"+expected+"'\n")
