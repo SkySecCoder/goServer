b403bcb3dcac5d2a1e49d76f3bec29d948e716a2
SkySecCoder <prasad.aakash@gmail.com>
2020-01-16 12:04:00 +0530

    modifying base.go

--- goServer/base/base.go
+++ goServer/base/base.go
@@ -10,7 +10,10 @@ func Base(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	log.Info("[+] "+r.Method+" Request at : "+path)
 	message := "<h1>Placeholder goServer main page...</h1>"
-	data, _ := ioutil.ReadAll("base.html")
+
+	file, _ := os.Open("base.html")
+	data, _ := ioutil.ReadAll(file)
+
 	message = string(data)
 	w.Write([]byte(message))
 }
