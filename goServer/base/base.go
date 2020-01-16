851c3480529fcff2e265192777b67ade0cfc6560
SkySecCoder <prasad.aakash@gmail.com>
2020-01-16 12:02:36 +0530

    modifying base.go

--- goServer/base/base.go
+++ goServer/base/base.go
@@ -3,11 +3,14 @@ package base
 import (
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"io/ioutil"
 )
 
 func Base(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	log.Info("[+] "+r.Method+" Request at : "+path)
 	message := "<h1>Placeholder goServer main page...</h1>"
+	data, _ := ioutil.ReadAll("base.html")
+	message = string(data)
 	w.Write([]byte(message))
 }
