3abef627185d7b906d4c6046fe65a433b0f2a62f
SkySecCoder <prasad.aakash@gmail.com>
2020-01-18 09:39:16 +0530

    Giving 404 for unknown route

--- goServer/base/base.go
+++ goServer/base/base.go
@@ -5,16 +5,24 @@ import (
 	"net/http"
 	"io/ioutil"
 	"os"
+	"encoding/json"
+	"strings"
 )
 
 func Base(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	log.Info("[+] "+r.Method+" Request at : "+path)
-	message := "<h1>Placeholder goServer main page...</h1>"
+	message := ""
+	if strings.ContainsAny(path[1:len(path)], "/") {
+		w.WriteHeader(http.StatusNotFound)
+		response := map[string]string{"Error":"404 Page not found..."}
+		data,_ := json.MarshalIndent(response, "", "    ")
+		message = string(data)
+	} else {
+		file, _ := os.Open("base.html")
+		data, _ := ioutil.ReadAll(file)
+		message = string(data)
+	}
 
-	file, _ := os.Open("base.html")
-	data, _ := ioutil.ReadAll(file)
-
-	message = string(data)
 	w.Write([]byte(message))
 }
